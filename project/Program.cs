f9a29c39ad1383392cacb02231837a86740419cf
Rohit <rohit2.joshi@ni.com>
2024-02-13 05:50:30 +0000

    working dev container

--- project/Program.cs
+++ project/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.Configure(app =>
+                    {
+                        app.UseRouting();
+
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapGet("/", async context =>
+                            {
+                                await context.Response.WriteAsync("Hello, World!");
+                            });
+                        });
+                    });
+                    webBuilder.UseUrls("http://0.0.0.0:8901");
+                })
+                .Build()
+                .RunAsync();
+        }
+    }
+}
